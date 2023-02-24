4f1d53de6c0472e2877c56e794c93cf72a4e3837
InfinityRoZe <anotherhafiz125@gmail.com>
2023-02-24 12:31:54 +0800

    Update ProjectASP.py

--- ProjectASP.py
+++ ProjectASP.py
@@ -5,6 +5,9 @@ import numpy as np
 # Loading the data file using pandas
 visitor_raw_df = pd.read_excel("Int Monthly Visitor.xlsx")
 
+# Displaying the loaded data
+print(visitor_raw_df)
+
 # give a name to the missing column name
 raw_col = list(visitor_raw_df.columns)
 raw_col[0] = "Year_Month"
@@ -15,20 +18,21 @@ visitor_raw_df.columns = raw_col
 visitor_raw_df["Year"] = visitor_raw_df["Year_Month"].str.split().str[0]
 visitor_raw_df["Month"] = visitor_raw_df["Year_Month"].str.split().str[1]
 
-# Displaying the loaded data
-print(visitor_raw_df.head())
-print(visitor_raw_df.columns)
-
 # select countries from Others Region column (include year column), put into new df
 OthersRegion_df = visitor_raw_df[[' USA ', ' Canada ', ' Australia ', ' New Zealand ', ' Africa ', 'Year']]
-
-print(OthersRegion_df)
+print(OthersRegion_df.head())
+print(OthersRegion_df.columns)
 
 # filter year
-
-
-# groupby year
+filter_df = OthersRegion_df[(OthersRegion_df['Year'] > "2007") & (OthersRegion_df['Year'] < "2018")]
+print(filter_df)
 
 # sum
+FilteredSumOthersRegion_df = filter_df.iloc[:, [0, 1, 2, 3, 4]]
+print(FilteredSumOthersRegion_df.sum())
 
 # sort by top 3
+top3_df = FilteredSumOthersRegion_df.sum(axis=0).sort_values(ascending= False)[:3]
+print(top3_df)
+
+# bar chart
